a37fd7fb0208fdba4dd1fef996428e68ed2a948d
wxcvm <wxcvm@users.noreply.github.com>
2026-08-14 12:25:19 +0800

    fix(cert): add cert-hash comparison to detect expired certs

--- app/src/main/java/org/adaway/util/WebServerUtils.java
+++ app/src/main/java/org/adaway/util/WebServerUtils.java
@@ -3,6 +3,7 @@ package org.adaway.util;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
