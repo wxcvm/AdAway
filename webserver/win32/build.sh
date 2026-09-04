77733f757435616e115f40d6ec9754d71660ac8c
wxcvm <133561272+wxcvm@users.noreply.github.com>
2026-09-05 00:15:49 +0800

    feat(v1.8): link iphlpapi

--- webserver/win32/build.sh
+++ webserver/win32/build.sh
@@ -22,7 +22,7 @@ gcc \
   jni/webserver.c jni/mongoose/mongoose.c "$HERE/gui_win32.c" "$HERE/app_res.o" \
   -L"$MINGW_PREFIX/lib" \
   -lssl -lcrypto -lws2_32 -lwinmm -lpthread \
-  -lgdi32 -luser32 -lshell32 -ladvapi32 -lcrypt32
+  -lgdi32 -luser32 -lshell32 -ladvapi32 -lcrypt32 -liphlpapi
 
 echo "==> Bundling runtime DLLs ..."
 for dll in libssl-3-x64.dll libcrypto-3-x64.dll zlib-1.dll \
